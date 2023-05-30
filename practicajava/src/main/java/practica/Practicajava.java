d82b160ed8de4de98a057ea22e2b72b7d6c54ad4
NATALY <natalypradomoreno@gmail.com>
2023-05-30 15:52:58 -0300

    agregue nuevo proyecto de java a repositorio

---
+++ practicajava/src/main/java/practica/Practicajava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package practica;
+
+/**
+ *
+ * @author Nataly
+ */
+public class Practicajava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
