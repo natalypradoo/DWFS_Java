65ed9112bd1b707fe398f281f9215d2b25f429ea
NATALY <natalypradomoreno@gmail.com>
2023-06-02 00:46:30 -0300

    creación de nuevas classes para practicar java
    de open bootcamp

---
+++ practicajava/src/main/java/bootcamp/HelloWord.java
@@ -0,0 +1,11 @@
+
+package bootcamp;
+public class HelloWord {
+    
+    // public= publico, static= pertenece a esta clase (no hace falta crear objet), 
+    // void(tipo de retorno, no vuelve nada, main=nombre, args = array de argumentos, puede recibir datos
+    
+    public static void main(String[]args){
+        System.out.println("Hola mundo desde claas Hello World");
+    }
+}
---
+++ practicajava/src/main/java/bootcamp/TipoDatosVariables.java
@@ -0,0 +1,20 @@
+package bootcamp;
+
+public class TipoDatosVariables {
+    
+    public static void main(String[]args){
+        // tipo idenntificador = 30;
+        // tipo identificador;
+        // identificador = 30;
+        
+        //enteros
+        byte number1 = 1; // 1 byte
+        short number2 = 2; // 2 bytes
+        int number3 = 3; // 4 bytes, mas usado
+        long number4 = 4; // 8 bytes, mas usado 
+        
+        //punto flotante
+        
+        
+    }
+}
